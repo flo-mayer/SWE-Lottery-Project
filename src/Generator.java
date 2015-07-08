d1e4f6bf9a7ef73e7830474f26113ab78d6db35d
Florian Mayer <flo-mayer@web.de>
2015-07-08 11:49:30 +0200

    X

--- src/Generator.java
+++ src/Generator.java
@@ -1,15 +1,19 @@
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 
 public class Generator {
 	static int[] drawing = new int[6];
 	
-	static void startgenerator(){
+	static void startgenerator() throws IOException{
 		ArrayList<Integer> drawing = new ArrayList<Integer>();
 		for(int i=1; i<=49; i++){
 			drawing.add(i);
 		}
 		Collections.shuffle(drawing);
 		Storage.storedraw(drawing.get(0), drawing.get(1), drawing.get(2), drawing.get(3), drawing.get(4), drawing.get(5));
+		Main.setdrawingused(1);
+		System.out.println("The numbers have been drawn!");
+		Main.showmenu();
 	}
 }
